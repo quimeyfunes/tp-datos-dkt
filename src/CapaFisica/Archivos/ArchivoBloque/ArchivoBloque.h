c0f6043587670f41879e06e33e8985b2049d60ee
marianknot92@gmail.com <marianknot92@gmail.com@583faab1-8ff4-330b-46ab-0f14db74188c>
2013-10-17 04:40:12 +0000

    subo varias cosas, el ArchivoBloque ya deberia estar listo para usar (si falta algo o hay algun problema me avisan) fijense q subo la clase Excepcion, para hacer que las excepciones de las 3 capas herenden de ahi, pero que cada capa maneje sus propias excepciones por separado ( tiene un metodo mostrarMensaje para usar en el catch)

--- src/CapaFisica/Archivos/ArchivoBloque/ArchivoBloque.h
+++ src/CapaFisica/Archivos/ArchivoBloque/ArchivoBloque.h
@@ -11,26 +11,32 @@
 #include <vector>
 #include <iostream>
 #include "../Archivo.h"
+#include "../../Excepciones/ExcepcionBloqueInexistente.h"
 using namespace std;
 
 class ArchivoBloque: Archivo {
 public:
-	ArchivoBloque(string nombre);
+	ArchivoBloque(string nombre, int tamanioBloque);
 	virtual ~ArchivoBloque();
 
-	void subir(char* bloque, int tamanioBloque);
-	void leer(char* &bloque, int tamanioBloque, int numBloque);
-	void borrarBloque(int numeroBloque);
+	int escribir(char* bloque);
+	char* leer(int numBloque);
+
+	//puede lanzar ExcepcionBloqueInexistente
+	void borrar(unsigned int numBloque);
+
+	int getCantidadBloques();
+
+	//pasar tamanioBloque por config
 
 private:
-
 	string nombreArchivo;
 	vector<char> vectorMapaBits;
 	int siguientePosicionLibre();
 	void leerMapaDeBits();
 	void escribirMapaBits();
+	int tamanioBloque;
 
 };
-
 
 #endif /* ARCHIVOBLOQUE_H_ */
