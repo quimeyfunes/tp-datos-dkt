85d7bd6707c628e7a525703849d4b6502a5b0a0c
duarte.mati@gmail.com <duarte.mati@gmail.com@583faab1-8ff4-330b-46ab-0f14db74188c>
2013-10-18 16:10:29 +0000

    Modificacion de la clase registroArbol y se agregan mas clases correspondientes al arbol

---
+++ trunk/src/CapaLogica/ArbolBMas/ArbolBMas.h
@@ -0,0 +1,23 @@
+/*
+ * ArbolBMas.h
+ *
+ *  Created on: 18/10/2013
+ *      Author: matias
+ */
+
+#ifndef ARBOLBMAS_H_
+#define ARBOLBMAS_H_
+
+#include <iostream>
+#include "string.h"
+#include <stdlib.h>
+
+using namespace std;
+
+class ArbolBMas {
+public:
+	ArbolBMas();
+	virtual ~ArbolBMas();
+};
+
+#endif /* ARBOLBMAS_H_ */
