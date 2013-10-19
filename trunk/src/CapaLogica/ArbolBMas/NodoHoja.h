0e65b4e7b849bdc6593d5f11cc53af5aaa346eb4
juanmabaracat@gmail.com <juanmabaracat@gmail.com@583faab1-8ff4-330b-46ab-0f14db74188c>
2013-10-19 21:43:12 +0000

    Actualicé las clases de nodos, y agregué el main para que que deje de tirar error al compilar

--- trunk/src/CapaLogica/ArbolBMas/NodoHoja.h
+++ trunk/src/CapaLogica/ArbolBMas/NodoHoja.h
@@ -12,8 +12,43 @@
 
 class NodoHoja: public Nodo {
 public:
-	NodoHoja();
-	virtual ~NodoHoja();
+		NodoHoja();
+        NodoHoja(ArchivoBloque * arch);
+        virtual ~NodoHoja();
+        void persistir(ArchivoBloque *archivo);
+        int agregar(Clave clave, string valor);
+        int agregar(RegistroArbol * reg);
+        int agregar(list<RegistroArbol*> *lista);
+        int baja (Clave clave, string valor);
+
+        bool tieneLaClave(Clave clave);
+        bool overflow();
+        bool underflow();
+        bool capacidadMinima();
+        bool isEmpty();
+        int tamanioOcupado();
+        void mostrar();
+
+        int cantidadDeRegistros();
+        string buscarClave(Clave clave);
+        static NodoHoja* hidratar(char* bloque, unsigned int indice);
+        static NodoHoja* cargar(ArchivoBloque* archivo, unsigned int indice);
+
+        void setSiguiente (unsigned int nrobloque);
+        void setClavesYDatos(list<RegistroArbol*> * lista);
+
+        unsigned int getSiguiente();
+        RegistroArbol* getRegistro(Clave clave);
+        string getPrimerClave();
+        string getClaveDelMedio();
+        list<RegistroArbol*> * getClavesYDatos();
+        list<RegistroArbol*> * getMitadDerecha();
+private:
+        int persistirRegistros(char * bloque);
+
+        unsigned int referencia_al_siguiente;
+        list<RegistroArbol*> * claves_datos;
 };
+
 
 #endif /* NODOHOJA_H_ */
--- trunk/src/CapaLogica/ArbolBMas/NodoInterno.h
+++ trunk/src/CapaLogica/ArbolBMas/NodoInterno.h
@@ -21,8 +21,51 @@ private:
 	list<Clave> claves;
 
 public:
-	NodoInterno();
-	virtual ~NodoInterno();
+                NodoInterno();
+        NodoInterno(ArchivoBloque * arch);
+        virtual ~NodoInterno();
+
+        int alta(string cancion, string valor);
+        void persistir(ArchivoBloque* archivo);
+
+        static NodoInterno* hidratar(char* bloque, unsigned int indice);
+        static NodoInterno* cargar(ArchivoBloque* archivo, unsigned int indice);
+        bool overflow();
+        bool underflow();
+        bool capacidadMinima();
+        bool isEmpty();
+        int tamanioOcupado();
+
+        void mostrar();
+        int cantidadDeClaves();
+        void agregarReferencia(Clave clave, unsigned int nodo);
+
+        void PartirNodoADerecha(NodoInterno* hermano_derecho, NodoInterno* padre);
+        void agregarClave(Clave clave);
+        void borrarClave(Clave clave);
+        void borrarClave(int indice);
+        void borrarReferencia(unsigned int hijo);
+        void agregarHijoAlFinal(unsigned int hijo);
+        void agregarClaves(list<Clave> lista_claves);
+        void agregarHijos(list<unsigned int> lista_hijos);
+        unsigned int buscarNodo(int indice);
+        int buscarClave(Clave clave);
+
+        void setReferenciaAIzq(Nodo * nodo);
+        void setReferenciaADer(Nodo * nodo);
+
+        list<Clave> * getMitadDerechaClaves();
+        list<unsigned int> * getMitadDerechaHijos();
+        Clave getClaveDelMedio();
+        unsigned int getUltimoNodo();
+        unsigned int getNodoAnteriorA(unsigned int indice);
+        list<Clave> getClaves();
+        list<unsigned int> getHijos();
+        Clave getClave(int index);
+        void setHijos(list<unsigned int> hijos);
+        void setClaves(list<Clave> claves);
+        Clave getPrimerClave();
+        Clave getUltimaClave();
 };
 
 #endif /* NODOINTERNO_H_ */
