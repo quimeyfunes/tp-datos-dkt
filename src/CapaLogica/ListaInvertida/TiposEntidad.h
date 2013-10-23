b24124043b7a1afed3ed6d9351d7420bcdb8fa25
quimey.funes@gmail.com <quimey.funes@gmail.com@583faab1-8ff4-330b-46ab-0f14db74188c>
2013-10-23 04:23:54 +0000

    -Constantes para la lista invertida

---
+++ src/CapaLogica/ListaInvertida/TiposEntidad.h
@@ -0,0 +1,11 @@
+#ifndef TIPOSENTIDAD_H_
+#define TIPOSENTIDAD_H_
+
+namespace TiposEntidad{
+
+	const static int CONSULTA = 1;
+	const static int CATEGORIA = 2;
+	const static int SERVICIO = 3;
+};
+
+#endif /*TIPOSENTIDAD_H_*/
