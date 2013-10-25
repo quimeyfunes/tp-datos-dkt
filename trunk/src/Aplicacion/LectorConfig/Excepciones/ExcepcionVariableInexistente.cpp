a5acf3d3efea61de336bb2d19e7d0b839de013a8
marianknot92@gmail.com <marianknot92@gmail.com@583faab1-8ff4-330b-46ab-0f14db74188c>
2013-10-25 16:19:02 +0000

    ya termine el parser, adentro del .h hay un ejemplo de como usarlo.. cualquier cosa me preguntan!

---
+++ trunk/src/Aplicacion/LectorConfig/Excepciones/ExcepcionVariableInexistente.cpp
@@ -0,0 +1,14 @@
+/*
+ * ExcepcionVariableInexistente.cpp
+ *
+ *  Created on: 25/10/2013
+ *      Author: marian
+ */
+
+#include "ExcepcionVariableInexistente.h"
+
+ExcepcionVariableInexistente::ExcepcionVariableInexistente() {
+
+	this->mensajeError = "La variable buscada no existe en el archivo";
+}
+
