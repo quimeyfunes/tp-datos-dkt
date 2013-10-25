8a5d30b3fa98f1009d72743d8833476cfd048d7e
marianknot92@gmail.com <marianknot92@gmail.com@583faab1-8ff4-330b-46ab-0f14db74188c>
2013-10-25 16:19:02 +0000

    ya termine el parser, adentro del .h hay un ejemplo de como usarlo.. cualquier cosa me preguntan!

---
+++ src/Aplicacion/LectorConfig/LectorConfig.cpp
@@ -0,0 +1,91 @@
+/*
+ * LectorConfig.cpp
+ *
+ *  Created on: 25/10/2013
+ *      Author: marian
+ */
+
+#include "LectorConfig.h"
+
+LectorConfig* LectorConfig::pLector = 0;
+
+LectorConfig* LectorConfig::getLector(string ruta){
+
+	if(pLector == 0){ //si nunca se llamó lo creo
+		 pLector = new LectorConfig(ruta);
+	}
+	return pLector;
+}
+
+LectorConfig::LectorConfig(string rutaArchivo) {
+
+	tamanioLinea = 100;
+	ifstream archivo;
+
+	archivo.open(rutaArchivo.c_str());
+	if(!archivo) throw new ExcepcionArchivoInexistente();
+
+	this->leerArchivoConfig(archivo);
+
+	archivo.close();
+}
+
+LectorConfig::~LectorConfig() {
+
+	vectorDatos.~vector();
+}
+
+string LectorConfig::getValor(string nombre){
+
+	unsigned int i;
+	for(i = 0; (i< vectorDatos.size())&&(vectorDatos[i].nombreVariable != nombre); i++){};
+
+	//si llego al final del vector, la variable buscada no existe
+	if(i == vectorDatos.size()) throw new ExcepcionVariableInexistente();
+
+	return vectorDatos[i].valorVariable;
+}
+
+void LectorConfig::leerArchivoConfig(ifstream &archivo){
+
+	Dato dato;
+	char linea[tamanioLinea];
+
+	archivo.getline(linea, tamanioLinea, '\n');
+
+	while(!archivo.eof()){
+
+		if(linea[0] != '#'){
+			dato = parsearLinea(linea);
+			if(dato.nombreVariable != "#ERROR#") vectorDatos.push_back(dato);
+		}
+		archivo.getline(linea, tamanioLinea, '\n');
+
+	};
+}
+
+int LectorConfig::stringToInt(const string& valor){
+	int retorno;
+	istringstream stream(valor);
+	return stream>>retorno? retorno:-1;
+}
+
+Dato LectorConfig::parsearLinea(char linea[]){
+
+	string nombreVariable= "";
+	string valorVariable= "";
+	Dato dato;
+	int i;
+
+	for(i=0; ((linea[i] != '=')&& (i< tamanioLinea)) ; i++)
+		nombreVariable += linea[i];
+
+	if( i== tamanioLinea) nombreVariable = "#ERROR#"; //no se encontro el signo de separacion
+
+	for(int j=i+1 ; j < tamanioLinea; j++)
+		valorVariable += linea[j];
+
+	dato.nombreVariable = nombreVariable;
+	dato.valorVariable = valorVariable;
+	return dato;
+}
---
+++ src/Aplicacion/LectorConfig/LectorConfig.h
@@ -0,0 +1,78 @@
+/*
+ * LectorConfig.h
+ *
+ *  Created on: 25/10/2013
+ *      Author: marian
+ */
+
+#ifndef LECTORCONFIG_H_
+#define LECTORCONFIG_H_
+
+#include "Excepciones/ExcepcionArchivoInexistente.h"
+#include "Excepciones/ExcepcionVariableInexistente.h"
+
+#include <vector>
+#include <string.h>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+typedef struct{
+	string nombreVariable;
+	string valorVariable;
+}Dato;
+
+class LectorConfig {
+
+public:
+	//se instancia un nuevo lector con el metodo getLector (asi se asegura el Singleton)
+	static LectorConfig* getLector(string rutaArchivo);
+
+	//le pasas como parametro el nombre de una variable dentro del archivo y te devuelve un string con su valor
+	string getValor(string nombreVariable);
+
+	//para convertir de string a int
+	int stringToInt(const string& valor);
+	virtual ~LectorConfig();
+
+private:
+	static LectorConfig* pLector;
+	LectorConfig(string rutaArchivoConfig);
+
+	int tamanioLinea;
+	vector<Dato> vectorDatos;
+	void leerArchivoConfig(ifstream &archivo);
+	Dato parsearLinea(char linea[]);
+};
+
+/*EJEMPLO DE ARCHIVO CONFIG:
+ *
+ * #si una linea empieza con numeral, es un comentario (no se parsea)
+ *
+ * #ejemplo de variable: nombreVariable=valorVariable
+ *
+ * tamanioBloque=2048
+ * textoRandom=estaCadenaSeVaAParsearComoString
+ * numeroDeNodos=10
+ * mensajeError=No se pudo realizar el pedido
+ *
+ *
+ * #comentario
+ * #las variables string van sin comillas
+ *
+ * se carga el lector haciendo:
+ *
+ * LectorConfig* pLector = LectorConfig::getLector("rutaYNombreArchivoConfig");
+ *
+ * si quiero levantar la variable tamanioBloque hago:
+ *
+ * string tamanio = pLector->getValor("tamanioBloque");
+ * int tamanioBloque = pLector->stringToInt(tamanio);
+ *
+ * string texto = pLector->getValor("mensajeError");
+ *
+ * cout<<texto; -----> No se pudo realizar el pedido
+ */
+
+#endif /* LECTORCONFIG */
