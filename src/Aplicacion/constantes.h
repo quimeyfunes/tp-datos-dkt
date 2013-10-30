8086e9f5bf3e6d9ab33eacc59df4d0a60ae101bb
quimey.funes@gmail.com <quimey.funes@gmail.com@583faab1-8ff4-330b-46ab-0f14db74188c>
2013-10-30 05:06:06 +0000

    -Agrego el diccionario de stop words y la clase diccionario. Tiene un metodo para saber si cualquier palabra es stop word en espanol, y ya esta funcionando. Cuando se instancia el diccionario se cargan las stop words en memoria en un map de c++, asi las busquedas son rapidas, para saber si una palabra es o no stop word.

--- src/Aplicacion/constantes.h
+++ src/Aplicacion/constantes.h
@@ -16,6 +16,7 @@ const unsigned int TAMANIOCLAVE_DEFAULT = 50;
 
 const string rutaConfig_ArchivoBloque = "\Aplicacion/config";
 const string rutaConfig_Nodo = "\Aplicacion/config";
+const string rutaConfig_Dic = "../Aplicacion/Diccionario/stop-words-espanol";
 
 const char separadorCamposClave = '#';
 const char separadorCamposEntidades = '#';
